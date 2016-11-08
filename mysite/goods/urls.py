9211412b81840921a2083da88193059789f9c485
jdh <jaebul2006@naver.com>
2016-11-08 16:50:42 +0900

    polls delete finish

--- mysite/goods/urls.py
+++ mysite/goods/urls.py
@@ -7,5 +7,5 @@ app_name = 'goods'
 urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^test/$', views.test, name='test'),
-    url(r'^roll_dice/(?P<goods_id>[0-9]+)/$', views.roll_dice, name='roll_dice'),
+    url(r'^roll_dice/(?P<goods_id>[0-9]+)/$', views.detail, name='detail'),
 ]
